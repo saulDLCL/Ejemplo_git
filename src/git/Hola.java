acf998b586743b3f6cf25c5ad356b6fa7ef3972a
saul <saulchico2@gmail.com>
2025-02-13 17:41:52 -0600

    primer comit - subiendo proyecto

---
+++ src/git/Hola.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git;
+
+/**
+ *
+ * @author saul
+ */
+public class Hola {
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
